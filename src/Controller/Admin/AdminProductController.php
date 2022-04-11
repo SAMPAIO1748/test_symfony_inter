165c517a69ad32be337d6a4fc27b8c686d37688c
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-04-11 15:06:04 +0200

    initialisation

---
+++ src/Controller/Admin/AdminProductController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Admin\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminProductController extends AbstractController
+{
+}
---
+++ src/Controller/Front/FrontProductController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Front\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontProductController extends AbstractController
+{
+}
