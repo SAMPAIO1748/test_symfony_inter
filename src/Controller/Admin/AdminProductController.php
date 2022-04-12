5614837f6b52774f1a4cae5ef54597473b066850
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-04-12 10:09:14 +0200

    CRUD product

--- src/Controller/Admin/AdminProductController.php
+++ src/Controller/Admin/AdminProductController.php
@@ -2,13 +2,93 @@
 
 namespace App\Controller\Admin;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminProductController extends AbstractController
 {
     /**
      * @Route("/admin/products", name="admin_product_list")
      */
+    public function adminProductList(ProductRepository $productRepository)
+    {
+        $products = $productRepository->findAll();
+
+        return $this->render('admin/product_list.html.twig', ['products' => $products]);
+    }
+
+    /**
+     * @Route("admin/product/{id}", name="admin_product_show")
+     */
+    public function adminProductShow($id, ProductRepository $productRepository)
+    {
+        $product = $productRepository->find($id);
+
+        return $this->render('admin/product_show.html.twig', ['product' => $product]);
+    }
+
+    /**
+     * @Route("admin/create/product", name="admin_product_create")
+     */
+    public function adminCreateProduct(Request $request, EntityManagerInterface $entityManagerInterface)
+    {
+        $product = new Product();
+
+        $productForm = $this->createForm(ProductType::class, $product);
+
+        $productForm->handleRequest($request);
+
+        if ($productForm->isSubmitted() && $productForm->isValid()) {
+            $entityManagerInterface->persist($product);
+            $entityManagerInterface->flush();
+
+            return $this->redirectToRoute("admin_product_list");
+        }
+
+        return $this->render("admin/product_form.html.twig", ['productForm' => $productForm->createView()]);
+    }
+
+    /**
+     * @Route("admin/update/product/{id}", name="admin_product_update")
+     */
+    public function adminProductUpdate(
+        $id,
+        EntityManagerInterface $entityManagerInterface,
+        Request $request,
+        ProductRepository $productRepository
+    ) {
+        $product = $productRepository->find($id);
+
+        $productForm = $this->createForm(ProductType::class, $product);
+
+        $productForm->handleRequest($request);
+
+        if ($productForm->isSubmitted() && $productForm->isValid()) {
+            $entityManagerInterface->persist($product);
+            $entityManagerInterface->flush();
+
+            return $this->redirectToRoute("admin_product_list");
+        }
+
+        return $this->render("admin/product_form.html.twig", ['productForm' => $productForm->createView()]);
+    }
+
+    /**
+     * @Route("admin/delete/product/{id}", name="admin_product_delete")
+     */
+    public function adminDeleteProduct($id, EntityManagerInterface $entityManagerInterface, ProductRepository $productRepository)
+    {
+        $product = $productRepository->find($id);
+
+        $entityManagerInterface->remove($product);
+        $entityManagerInterface->flush();
+
+        return $this->redirectToRoute('admin_product_list');
+    }
 }
--- src/Controller/Front/FrontProductController.php
+++ src/Controller/Front/FrontProductController.php
@@ -2,10 +2,30 @@
 
 namespace App\Controller\Front;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FrontProductController extends AbstractController
 {
+    /**
+     * @Route("/products/", name="front_product_list")
+     */
+    public function frontProductList(ProductRepository $productRepository)
+    {
+        $products = $productRepository->findAll();
+
+        return $this->render('front/product_list.html.twig', ['products' => $products]);
+    }
+
+    /**
+     * @Route("product/{id}", name="front_product_show")
+     */
+    public function frontProductShow(ProductRepository $productRepository, $id)
+    {
+        $product = $productRepository->find($id);
+
+        return $this->render('front/product_show.html.twig', ['product' => $product]);
+    }
 }
