dbb69a5f5bd377f736a1174e2e09f8a0e658c298
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-04-13 13:55:05 +0200

    CRUD media

--- src/Controller/Admin/AdminMediaController.php
+++ src/Controller/Admin/AdminMediaController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Media;
 use App\Form\MediaType;
+use App\Repository\MediaRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -66,5 +67,101 @@ class AdminMediaController extends AbstractController
         }
 
         return $this->render("admin/media_form.html.twig", ['mediaForm' => $mediaForm->createView()]);
+    }
+
+    //Exercice : Faire le CRUD complet pour media
+
+    /**
+     * @Route("/admin/medias/", name="list_media")
+     */
+    public function listMedia(MediaRepository $mediaRepository)
+    {
+        $images = $mediaRepository->findAll();
+
+        return $this->render("admin/media_list.html.twig", ['images' => $images]);
+    }
+
+    /**
+     * @Route("/admin/media/{id}", name="show_media")
+     */
+    public function showMedia($id, MediaRepository $mediaRepository)
+    {
+        $image = $mediaRepository->find($id);
+
+        return $this->render("admin/media_show.html.twig", ['image' => $image]);
+    }
+
+    /**
+     * @Route("/admin/update/media/{id}", name="media_update")
+     */
+    public function updateMedia(
+        Request $request,
+        EntityManagerInterface $entityManagerInterface,
+        SluggerInterface $sluggerInterface,
+        MediaRepository $mediaRepository,
+        $id
+    ) {
+        $media = $mediaRepository->find($id);
+
+        $mediaForm = $this->createForm(MediaType::class, $media);
+
+        $mediaForm->handleRequest($request);
+
+        if ($mediaForm->isSubmitted() && $mediaForm->isValid()) {
+
+            // On récupère le fichier que l'on veut enregistrer
+            $mediaFile = $mediaForm->get('src')->getData();
+
+            if ($mediaFile) {
+
+                // On va crée un nom unique et valide à notre fichier à partir du nom original
+                // pour éviter tout problème de confusion.
+
+                // On récupère le nom original du fichier.
+                $originalFielname = pathinfo($mediaFile->getClientOriginalName(), PATHINFO_FILENAME);
+
+                // On utilise le slug sur le nom originzl pour avoir un nom valide du fichier.
+                $safeFilename = $sluggerInterface->slug($originalFielname);
+
+                // On ajoute un id unique au nom du fichier
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $mediaFile->guessExtension();
+
+                // On déplace le fichier dans le dossier public/media
+                // la destination du fichier est enregistré dans 'images_directory'
+                // qui est défini dans la fichier config\services.yaml.
+
+                $mediaFile->move(
+                    $this->getParameter('images_directory'),
+                    $newFilename
+                );
+
+                $media->setSrc($newFilename);
+            }
+
+            $entityManagerInterface->persist($media);
+
+            $entityManagerInterface->flush();
+
+            return $this->redirectToRoute("admin_product_list");
+        }
+
+        return $this->render("admin/media_form.html.twig", ['mediaForm' => $mediaForm->createView()]);
+    }
+
+    /**
+     * @Route("admin/delete/media/{id}", name="delete_media")
+     */
+    public function deleteMedia(
+        $id,
+        EntityManagerInterface $entityManagerInterface,
+        MediaRepository $mediaRepository
+    ) {
+        $media = $mediaRepository->find($id);
+
+        $entityManagerInterface->remove($media);
+
+        $entityManagerInterface->flush();
+
+        return $this->redirectToRoute("list_media");
     }
 }
